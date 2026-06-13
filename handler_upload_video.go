5a4f5f0f2d9d1c5d3afd46f6314e21d9dc73e260
pawelbalcerek <pawbal96@gmail.com>
2026-06-13 18:48:28 +0200

    presigned s3 url

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -123,10 +123,16 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	video.VideoURL = cfg.s3AssetURL(key)
+	video.VideoURL = ptr(key) // cfg.s3AssetURL(key)
 
 	if err := cfg.db.UpdateVideo(video); err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Failed to update video", err)
+		return
+	}
+
+	video, err = cfg.dbVideoToSignedVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to sign video", err)
 		return
 	}
 
